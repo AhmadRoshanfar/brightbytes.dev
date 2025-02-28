0dba9343ae2a2b691fcd304969b6140e42ffcb5f
AhmadRoshanfar <ahmadroshanfar@example.com>
2025-02-28 16:40:36 -0500

    Updates

---
+++ 4577d2ec-598ab9f0798bccada4fa.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbrightbytes_dev=self.webpackChunkbrightbytes_dev||[]).push([[817],{1121:function(t,e,h){h.d(e,{usP:function(){return r}});var n=h(3621);function r(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"m18.73 5.41-1.28 1L12 10.46 6.55 6.37l-1.28-1A2 2 0 0 0 2 7.05v11.59A1.36 1.36 0 0 0 3.36 20h3.19v-7.72L12 16.37l5.45-4.09V20h3.19A1.36 1.36 0 0 0 22 18.64V7.05a2 2 0 0 0-3.27-1.64z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=4577d2ec-598ab9f0798bccada4fa.js.map
